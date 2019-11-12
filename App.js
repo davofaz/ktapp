d43e47e63886dc3a93f60d72966f312f2222917b
Gaizka Allende <gaizka@allende.org>
2019-11-12 17:31:06 +0000

    call require for ionicons

--- App.js
+++ App.js
@@ -34,6 +34,7 @@ export default function App(props) {
                 ]),
                 Font.loadAsync({
                   'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
+                  Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf")
                 }),
               ])
             }
