b57a310f15f860b2138109e02286a91df2154ef6
Gaizka Allende <gaizka@allende.org>
2019-11-12 17:16:25 +0000

    remove icon import in app

--- App.js
+++ App.js
@@ -3,7 +3,6 @@ import { Asset } from 'expo-asset';
 import * as Font from 'expo-font';
 import React, { useState } from 'react';
 import { Platform, StatusBar, StyleSheet, View } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
 
 import { Provider } from 'react-redux';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
@@ -34,10 +33,6 @@ export default function App(props) {
                   require('./assets/images/kt-logo.png'),
                 ]),
                 Font.loadAsync({
-                  // This is the font that we are using for our tab bar
-                  ...Ionicons.font,
-                  // We include SpaceMono because we use it in HomeScreen.js. Feel free to
-                  // remove this if you are not using it in your app
                   'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
                 }),
               ])
