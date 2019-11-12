6cc347af4405417667a47b660217ea9dc579f75c
Gaizka Allende <gaizka@allende.org>
2019-11-12 16:23:58 +0000

    fix to app loading async

--- App.js
+++ App.js
@@ -25,9 +25,23 @@ export default function App(props) {
   if (!isLoadingComplete && !props.skipLoadingScreen) {
     return (
       <AppLoading
-        startAsync={loadResourcesAsync}
-        onError={handleLoadingError}
-        onFinish={() => handleFinishLoading(setLoadingComplete)}
+        startAsync={
+          () =>  Promise.all([
+            Asset.loadAsync([
+              require('./assets/images/kt-logo.png'),
+              require('./assets/images/kt-logo.png'),
+            ]),
+            Font.loadAsync({
+              // This is the font that we are using for our tab bar
+              ...Ionicons.font,
+              // We include SpaceMono because we use it in HomeScreen.js. Feel free to
+              // remove this if you are not using it in your app
+              'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
+            }),
+          ])
+        }
+        onError={() => console.warn(error)}
+        onFinish={() => setLoadingComplete(true)}
       />
     );
   } else {
@@ -40,32 +54,6 @@ export default function App(props) {
       </Provider>
     );
   }
-}
-
-async function loadResourcesAsync() {
-  await Promise.all([
-    Asset.loadAsync([
-      require('./assets/images/kt-logo.png'),
-      require('./assets/images/kt-logo.png'),
-    ]),
-    Font.loadAsync({
-      // This is the font that we are using for our tab bar
-      ...Ionicons.font,
-      // We include SpaceMono because we use it in HomeScreen.js. Feel free to
-      // remove this if you are not using it in your app
-      'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
-    }),
-  ]);
-}
-
-function handleLoadingError(error) {
-  // In this case, you might want to report the error to your error reporting
-  // service, for example Sentry
-  console.warn(error);
-}
-
-function handleFinishLoading(setLoadingComplete) {
-  setLoadingComplete(true);
 }
 
 const styles = StyleSheet.create({
