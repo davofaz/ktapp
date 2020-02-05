b15882ab20241df90327084f75121653b2e177f8
David Fazaldin <dfazaldin@gmail.com>
2020-02-05 11:52:59 +0000

    Moved bottom Nav to own component and refactored all main screens to use it and cleaned up un used code

--- components/Screen.js
+++ components/Screen.js
@@ -4,16 +4,19 @@ import {
   View,
   ImageBackground,
   ScrollView,
+  Platform,
   StyleSheet,
+  TouchableHighlight,
+  Text,
 } from 'react-native';
-
+import * as WebBrowser from 'expo-web-browser';
 import Logo from './Logo';
 
 export default function Screen({
   title,
   children,
-  buttons,
 }) {
+
   return (
     <View style={styles.container}>
       <ImageBackground source={require('../assets/images/background-image.jpg')} style={{width: '100%', height: '100%', resizeMode: 'cover'}}>
@@ -25,27 +28,15 @@ export default function Screen({
             />
             {children}
         </ScrollView>
-        <View style={styles.bottomNav}>
-          {buttons}
-          <TouchableHighlight
-              style={styles.buttonSquare}
-              onPress={() => goBack()}
-              title="Go back"
-              underlayColor='rgba(250, 168, 127, 1)'>
-              <View style={styles.backButton}>
-                <Ionicons
-                  style={styles.arrowIcon}
-                  size={32}
-                  name={Platform.OS === 'ios'
-                  ? 'ios-arrow-back' : 'md-arrow-back'}/>
-                <Text style={styles.buttonText}>Go Back</Text>
-              </View>
-            </TouchableHighlight>
-          </View>
       </ImageBackground>
     </View>
   );
 }
+
+Screen.navigationOptions = {
+  header: null,
+
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -57,9 +48,62 @@ const styles = StyleSheet.create({
     flex: 1,
     height: '100%'
   },
+  bottomNav: {
+    width: '80%',
+    alignItems: 'center',
+    alignSelf: 'center',
+    flexDirection:'row',
+    marginBottom:10,
+    },
+  buttonSquare: {
+    padding: 15,
+    width: 110,
+    height: 110,
+    borderRadius: 10,
+    borderWidth:1,
+    borderColor: '#fff',
+    opacity: 80,
+    flexDirection: 'row',
+    backgroundColor: 'rgba(137, 167, 165, 1)',
+    marginTop: 35,
+    marginRight: 10,
+    marginLeft: 25,
+    justifyContent:'center',
+    alignContent: 'center',
+    shadowColor: 'rgba(0,0,0, .6)', // IOS
+    shadowOffset: { height: 1, width: 1 }, // IOS
+    shadowOpacity: 1, // IOS
+    shadowRadius: 1, //IOS
+    elevation: 6, // Android
+    },
+    bottomNav: {
+      width: '80%',
+      alignItems: 'center',
+      alignSelf: 'center',
+      flexDirection:'row',
+      marginBottom:10,
+      },
+    backButton: {
+      flex:1,
+      alignItems: 'center',
+      flexDirection:'column',
+    },
+    buttonText: {
+      color: '#fff',
+      fontSize: 14,
+      width: 80,
+      textAlign: 'center',
+      textTransform: 'uppercase',
+      fontWeight: 'bold',
+    },
+    arrowIcon: {
+      color:'#fff',
+      marginTop:15,
+    },
 });
 
 Screen.propTypes = {
   title: PropTypes.string,
+  seeMoreURL: PropTypes.string,
   children: PropTypes.oneOfType([ PropTypes.arrayOf(PropTypes.node), PropTypes.node ]).isRequired
 };
